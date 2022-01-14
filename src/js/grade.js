eafc54b090ac08a6192bcce3c95a3d42f03fa77e
Jakei <2435207533@qq.com>
2022-01-14 22:41:30 +0800

    修改后的三个页面脚本

---
+++ src/js/grade.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	$("#1").click(function(){
+		console.log(1);
+		$("#grade").html("<h5>1</h5>");
+	})
+	$("#2").click(function(){
+		$("#grade").html("<h5>2</h5>");
+	})
+	$("#3").click(function(){
+		$("#grade").html("<h5>3</h5>");
+	})
+	$("#4").click(function(){
+		$("#grade").html("<h5>4</h5>");
+	})
+	$("#5").click(function(){
+		$("#grade").html("<h5>5</h5>");
+	})
+	$("#6").click(function(){
+		$("#grade").html("<h5>6</h5>");
+	})
+	$("#7").click(function(){
+		$("#grade").html("<h5>7</h5>");
+	})
+	$("#8").click(function(){
+		$("#grade").html("<h5>8</h5>");
+	})
+	$("#9").click(function(){
+		$("#grade").html("<h5>9</h5>");
+	})
+	$("#10").click(function(){
+		$("#grade").html("<h5>10</h5>");
+	})
+})
